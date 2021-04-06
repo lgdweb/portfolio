82deeb356b8599bb6e901f7d2ead64811321c209
Titux Metal <github@lgdweb.fr>
2021-04-06 03:48:16 +0200

    Format js files in assets/js

--- src/assets/js/contactForm.js
+++ src/assets/js/contactForm.js
@@ -3,32 +3,26 @@
 const form = [...document.querySelector('.contact__form').elements]
 
 form.forEach(function (i) {
-  
-	let input = i.parentElement
-  
-	i.addEventListener('focus', function (e) {
-    
-		input.classList.add('is-focused')
-		input.classList.add('has-label')
-	})
-  
-	i.addEventListener('blur', function (e) {
-    
-		if (e.target.value === '') {
-      
-			input.classList.remove('has-label')
-		}
-    
-		input.classList.remove('is-focused')
-	})
-  
-	if (i.value !== '') {
-    
-		i.parentElement.classList.add('has-label')
-	}
-  
-	if (i.type === 'textarea') {
-    
-		i.parentElement.classList.add('contact__form--textarea')
-	}
+  let input = i.parentElement
+
+  i.addEventListener('focus', function (e) {
+    input.classList.add('is-focused')
+    input.classList.add('has-label')
+  })
+
+  i.addEventListener('blur', function (e) {
+    if (e.target.value === '') {
+      input.classList.remove('has-label')
+    }
+
+    input.classList.remove('is-focused')
+  })
+
+  if (i.value !== '') {
+    i.parentElement.classList.add('has-label')
+  }
+
+  if (i.type === 'textarea') {
+    i.parentElement.classList.add('contact__form--textarea')
+  }
 })
